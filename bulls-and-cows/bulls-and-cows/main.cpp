e96ba230b94d8be7af90145e01ceccae513a41b4
André Tonic <andre_tonic@imac-de-andre-tonic.local>
2017-12-13 21:11:09 +0100

    1st Commit

---
+++ bulls-and-cows/bulls-and-cows/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bulls-and-cows - First
+//
+//  Created by André Tonic on 13/12/2017.
+//  Copyright © 2017 André Tonic. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
